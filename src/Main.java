9dbec469b6ef00f19036087641fe77107ff5a598
Nikitos-Barbos <sobemov@gmail.com>
2024-11-10 15:55:35 +0300

    Работик

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,60 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        int year = 2000;
+        checkLeapAndPrint(year);
+
+        // Задание 2
+        int deviceYear = 2017;
+        int clientOs = 5;
+        boolean oldDevice = isOldDevice(deviceYear);
+        String resultMessage = "Установите ";
+
+        if (oldDevice) {
+            resultMessage += "Lite ";
+        }
+
+        resultMessage += "версию для ";
+
+        if (clientOs == 1) {
+            resultMessage += "Android";
+        } else {
+            resultMessage += "iOS";
+        }
+
+        System.out.println(resultMessage);
+
+        // Задание 3
+        int deliveryDistance = 788;
+        int deliveryDays = calculateDeliveryDays(deliveryDistance);
+        if (deliveryDays != -1) {
+            System.out.println("Потребуется дней: " + deliveryDays);
+        } else {
+            System.out.println("Доставка на данное расстояние невозможна.");
+        }
+    }
+
+    private static void checkLeapAndPrint(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.printf("%d год - високосный год%n", year);
+        } else {
+            System.out.printf("%d год - невисокосный год%n", year);
+        }
+    }
+
+    public static boolean isOldDevice(int year) {
+        return year < 2015;
+    }
+
+    public static int calculateDeliveryDays(int deliveryDistance) {
+        int result = -1;
+        if (deliveryDistance > 0 && deliveryDistance <= 20) {
+            result = 1;
+        } else if (deliveryDistance <= 60) {
+            result = 2;
+        } else if (deliveryDistance <= 100) {
+            result = 3;
+        }
+        return result;
     }
 }
